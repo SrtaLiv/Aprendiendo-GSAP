78a22a42dd249ea85066a81c14a5a22bc8f40fa1
olivia <olitutos712@gmail.com>
2025-08-12 23:00:57 -0300

    proyectos que habia comenzado

---
+++ Portfolio3D/AvatarCustomPart3/r3f-ultimate-character-configurator-part-3-main/README.md
@@ -0,0 +1,7 @@
+# React Three Fiber Ultimate Character Configurator
+
+## Part 3 - Model Composition
+
+![Video thumbnail](https://github.com/user-attachments/assets/932e4712-43f8-4e23-ab3f-5327639ee332)
+
+[Video tutorial](https://youtu.be/H69pv8HshWs)
---
+++ gsap/personaje-ojos-movimiento/ojitos/src/main.js
@@ -0,0 +1,38 @@
+import './style.css'
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+
+// Registra el plugin Flip
+gsap.registerPlugin(Flip);
+
+console.log("GSAP está funcionando con Yarn");
+
+let mouseX = 0;
+let mouseY = 0;
+
+let width = window.innerWidth; // ancho de la ventana
+let height = window.innerHeight; // alto de la ventana
+
+console.log(width, height);
+
+function updateMousePosition(e) {
+    mouseX = e.clientX; // posición del mouse en X
+    mouseY = e.clientY; // posición del mouse en Y
+}
+
+window.addEventListener("mousemove", updateMousePosition); // evento de movimiento del mouse
+console.log(mouseX, mouseY);
+
+function moveEyes() {
+    // Convertir la posición del ratón a un rango de -50 a 50
+    const x = ((mouseX / width) - 0.5) * 100;
+    const y = ((mouseY / height) - 0.5) * 100;
+
+    gsap.to(".eye", {
+        xPercent: x / 5,
+        yPercent: y / 5
+    });
+}
+
+// Ejecutar la función de animación en cada frame
+gsap.ticker.add(moveEyes);
